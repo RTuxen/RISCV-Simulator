cca36c78cde874cf6bad96904ad067f4f8681dd4
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-11-05 16:33:03 +0100

    Add files via upload

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 
 
@@ -6,16 +7,25 @@ public class Simulator {
 
     static int pc;
     static int reg[] = new int[31];
-    static String fileName = "addpos.bin";
+    static String fileName = "addlarge.bin";
 
 
 
     public static void main(String[] args) throws IOException {
 
         pc = 0;
-        int instr,opcode,rd,rs1, rs2, funct3, funct7, imm1, imm2, imm;
+        int instr,opcode,rd,rs1, rs2, funct3, funct7, imm1, imm2, imm,shamt;
+
 
         int[] progr = readBinaryFile(fileName);
+
+
+        for (int k=0; k<progr.length; k++){
+            instr = progr[k];
+            opcode = instr & 0x7f;
+            System.out.println("Opcode for instruction " + k +" er " + opcode);
+        }
+
 
 
         for (;;) {
@@ -23,21 +33,107 @@ public class Simulator {
             instr = progr[pc];
             opcode = instr & 0x7f;
             rd = (instr >> 7) & 0x1f;
-            imm1 = (instr >> 7) & 0x01f;
             funct3 = (instr >> 12) & 0x07;
             rs1 = (instr >> 15) & 0x1f;
             rs2 = (instr >> 20) & 0x1f;
             imm = (instr >> 20);
+            imm1 = (instr >> 7) & 0x01f;
             imm2 = (instr >> 25);
             funct7 = (instr >> 25);
+            shamt = (instr >> 20) & 0x01f;
 
             switch (opcode) {
 
                 case 0x13: // Opcode 0010011
-                    reg[rd] = reg[rs1] + imm;
+
+                    switch (funct3){
+                        case 0x00: // Addi
+                            reg[rd] = reg[rs1] + imm;
+                            break;
+                        case  0x01: // SLLI
+                            reg[rd] = reg[rs1] << shamt;
+                            break;
+                        case 0x02: // SLTI
+                            reg[rd] = (reg[rs1] < imm) ? 1 : 0;
+                            break;
+                        case 0x03: // SLTIU
+                            reg[rd] = (reg[rs1] < imm) ? 1 : 0;
+                            break;
+                        case 0x04: // XORI
+                            reg[rd] = reg[rs1] ^imm;
+                            break;
+                        case 0x05: // funct3 = 101
+                            switch (funct7){
+                                case 0x00: // SLRI
+                                    reg[rd] = reg[rs1] >> shamt;
+                                    break;
+                                case 0x20: // SRAI
+                                    reg[rd] = reg[rs1] >> shamt;
+                                    break;
+                            }
+                            break;
+                        case 0x06: // ORI
+                            reg[rd] = reg[rs1] | imm;
+                            break;
+                        case 0x07: // ANDI
+                            reg[rd] = reg[rs1] & imm;
+                            break;
+                        default:
+                            System.out.println("funct3" + funct3 + " has not been implemented yet");
+                    }
                     break;
-                case 0x33: // add
-                    reg[rd] = reg[rs1] + reg[rs2];
+
+                case 0x33: // Opcode 0110011
+
+                    switch (funct3) {
+                        case 0x00: //ADD or SUB
+                            switch (funct7) {
+                                case 0x00: //ADD
+                                    reg[rd] = reg[rs1] + reg[rs2];
+                                    break;
+                                case 0x20: //SUB
+                                    reg[rd] = reg[rs1] - reg[rs2];
+                                    break;
+                            }
+                            break;
+                        case 0x01: //SLL - Shift Left
+                            reg[rd] = reg[rs1] << reg[rs2];
+                            break;
+                        case 0x02: //SLT - Set Less Than
+                            reg[rd] = (reg[rs1] < reg[rs2]) ? 1 : 0;
+                            break;
+                        case 0x03: //SLTU - Set Less Than Unsigned
+                            reg[rd] = (reg[rs1] < reg[rs2]) ? 1 : 0;
+                            break;
+                        case 0x04: //XOR
+                            reg[rd] = reg[rs1] ^ reg[rs2];
+                            break;
+                        case 0x05: //SRL or SRA
+                            switch (funct7) {
+                                case 0x00://SRL - Shift Right
+                                    reg[rd] = reg[rs1] >> reg[rs2];
+                                    break;
+                                case 0x20://SRA - Shift Right Arithmetic
+                                    reg[rd] = reg[rs1] >> reg[rs2];
+                                    break;
+                            }
+                            break;
+                        case 0x06: //OR
+                            reg[rd] = reg[rs1] | reg[rs2];
+                            break;
+                        case 0x07: //AND
+                            reg[rd] = reg[rs1] & reg[rs2];
+                            break;
+
+                    }
+                    break;
+
+                case 0x37: // lui
+
+                    rd = (instr >> 7) & 0x01f;
+                    rs1 = (instr >> 15) & 0x01f;
+                    imm = (instr >> 20);
+                    reg[rd] = reg[rs1] + (imm << 20);
                     break;
                 case 0x73: // ecall
                     if (reg[10] == 10){
@@ -85,7 +181,7 @@ public class Simulator {
                         number[i] = number[i] & 0xFF;
                     }
                 }
-                System.out.println("\n" + number[0] + " " + number[1] + " "+ number[2] + " " + number[3] + " ");
+                //System.out.println("\n" + number[0] + " " + number[1] + " "+ number[2] + " " + number[3] + " ");
                 instruction[counter] = number[0] + (number[1] << 8) + (number[2] << 16) + (number[3] << 24);
                 counter++;
 
@@ -96,7 +192,7 @@ public class Simulator {
         }
         // Closes the file.
         inputFile.close();
-        System.out.println("   ***Done with reading from a binary file.");
+        System.out.println("\n   ***Done with reading from a binary file.");
 
         int[] instructionList = new int[counter]; // Loads instructions into array of correct size
         for (int j = 0; j<counter; j++){
