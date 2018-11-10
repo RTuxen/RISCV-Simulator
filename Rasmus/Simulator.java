e4fcbc545349bd144342303da02c4895297ca796
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-11-10 17:38:51 +0100

    Stoler ikke på imm_B, imm_J og imm_S

--- Rasmus/Simulator.java
+++ Rasmus/Simulator.java
@@ -7,32 +7,37 @@ public class Simulator {
 
     static int pc;
     static int reg[] = new int[31];
-    static String fileName = "branchcnt.bin";
+    static String fileName = "branchmany.bin";
 
 
 
     public static void main(String[] args) throws IOException {
 
         pc = 0;
-        int instr,opcode,rd,rs1, rs2, funct3, funct7, imm1, imm2, imm,shamt,Bimm;
+        int instr,opcode,rd,rs1, rs2, funct3, funct7, imm1, imm2, imm,shamt,imm_B,imm_J,imm_U,imm_S;
 
 
         int[] progr = readBinaryFile(fileName);
-        /*
-        for (int k = 0; k<progr.length; k++){
+
+        // Prints list of all instructions
+        System.out.println("List of Instructions");
+        for (int k = 0; k < progr.length; k++){
             System.out.println(String.format("0x%08X", progr[k]));
         }
 
+        /*
         instr = progr[3];
-        imm1 = (instr >>> 7) & 0x1f;
-        imm2 = (instr >>> 25) ;
+        imm1 = (instr >> 7) & 0x1f;
+        imm2 = (instr >> 25) ;
         Bimm = ((imm2 << 5) & 0x800) | ((imm1 << 11) & 0x400) | ((imm2 << 5) & 0x3E0) | (imm1 & 0x1E);
-        Bimm = (((((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0xFFFFF7FE) | ((   (((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0x00000001) << 11))/4;
-        System.out.println("imm1 = " + Integer.toBinaryString(imm1) + " imm2 = "+ Integer.toBinaryString(imm2));
+        Bimm = ((imm2 << 5) & 0x800) + ((imm1 << 10) & 0x400) | ((imm2 << 5) & 0x3E0) | (imm1 & 0x1E);
+        //Bimm = (((((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0xFFFFF7FE) | ((   (((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0x00000001) << 11))/4;
+        System.out.println("imm2 = "+ Integer.toBinaryString(imm2) + " imm1 = " + Integer.toBinaryString(imm1));
         System.out.println(imm1+" "+imm2);
         System.out.println(Bimm);
         System.out.println(Integer.toBinaryString(Bimm));
         */
+
 
 
         for (;;) {
@@ -48,10 +53,36 @@ public class Simulator {
             imm2 = (instr >> 25);
             funct7 = (instr >> 25);
             shamt = (instr >> 20) & 0x01f;
-            //Bimm = ((imm2 << 5) & 0x800) + ((imm1 << 11) & 0x400) + ((imm2 << 5) & 0x3E0) + (imm1 & 0x1E);
-            Bimm = (((((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0xFFFFF7FE) | (((((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0x00000001) << 11))/4;
+            //Bimm = ((imm2 << 5) & 0x800) + ((imm1 << 10) & 0x400) | ((imm2 << 5) & 0x3E0) | (imm1 & 0x1E);
+            imm_B = (((((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0xFFFFF7FE) | (((((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F)) & 0x00000001) << 11))/4;
+            imm_J = (((instr >> 20) & 0xFFF007FE) | ((instr >>> 9) & 0x00000800) | (instr & 0x000FF000));
+            imm_S = (((instr >> 20) & 0xFFFFFFE0) | ((instr >>> 7) & 0x0000001F));
+            imm_U = (instr >> 20) << 20;
+
+            ++pc; // We count in 4 byte words
 
             switch (opcode) {
+                case 0x03: // Opcode 0000011
+                    switch (funct3){
+                        case 0x00: // LB - Load Byte
+                            System.out.println("LB - Load Byte has not been implemented yet");
+                            break;
+                        case 0x01: // LH - Load Halfword
+                            System.out.println("LH - Load Halfword has not been implemented yet");
+                            break;
+                        case 0x02: // LW - Load Word
+                            System.out.println("LW - Load Word has not been implemented yet");
+                            break;
+                        case 0x04: // LBU - Load Byte Unsigned
+                            System.out.println("LBU - Load Byte Unsigned has not been implemented yet");
+                            break;
+                        case 0x05: // LHU - Load Halfword Unsigned
+                            System.out.println("Load Halfword Unsigned has not been implemented yet");
+                            break;
+                        default:
+                            System.out.println("For opcode 0x03, funct3" + funct3 + " has not been implemented yet");
+                    }
+                    break;
 
                 case 0x13: // Opcode 0010011
 
@@ -88,7 +119,27 @@ public class Simulator {
                             reg[rd] = reg[rs1] & imm;
                             break;
                         default:
-                            System.out.println("funct3" + funct3 + " has not been implemented yet");
+                            System.out.println("For opcode 0x13, funct3" + funct3 + " has not been implemented yet");
+                    }
+                    break;
+
+                case 0x17: // opcode 0010111 AUIPC - Add Upper Immediate to PC
+                    reg[rd] = pc + imm_U;
+                    break;
+
+                case 0x23: // Opcode 0100011
+                    switch (funct3){
+                        case 0x00: // SB - Store Byte
+                            System.out.println("SB - Store Byte has not been implemented yet");
+                            break;
+                        case 0x01: // SH - Store Halfword
+                            System.out.println("SH - Store Halfword has not been implemented yet");
+                            break;
+                        case 0x02: // SW - Store Word
+                            System.out.println("SW - Store Word has not been implemented yet");
+                            break;
+                        default:
+                            System.out.println("For opcode 0x23, funct3" + funct3 + " has not been implemented yet");
                     }
                     break;
 
@@ -136,48 +187,57 @@ public class Simulator {
 
                     }
                     break;
+
+                case 0x37: // opcode 0110111 LUI - Load Upper Immediate
+                    reg[rd] = reg[rs1] + imm_U;
+                    break;
+
                 case 0x63: //Opcode 1100011
 
                     switch (funct3) {
                         case 0x00: // BEQ - Branch Equal
                             if (reg[rs1] == reg[rs2]){
-                                pc = pc + Bimm - 1;
+                                pc = pc + imm_B - 1;
                             }
                             break;
                         case 0x01: // BNE - Branch Not Equal
                             if (reg[rs1] != reg[rs2]){
-                                pc = pc + Bimm - 1;
+                                pc = pc + imm_B - 1;
                             }
                             break;
                         case 0x04: // BLT - Branch Less Than
                             if (reg[rs1] < reg[rs2]){
-                                pc = pc + Bimm - 1;
+                                pc = pc + imm_B - 1;
                             }
                             break;
                         case 0x05: // BGE - Branch Greater Than or Equal
                             if (reg[rs1] >= reg[rs2]){
-                                pc = pc + Bimm - 1;
+                                pc = pc + imm_B - 1;
                             }
                             break;
                         case 0x06: // BLTU - Branch Less Than Unsigned
                             if (reg[rs1] < reg[rs2]){
-                                pc = pc + Bimm - 1;
+                                pc = pc + imm_B - 1;
                             }
                             break;
                         case 0x07: // BGEU - Branch >= Unsigned
                             if (reg[rs1] >= reg[rs2]){
-                                pc = pc + Bimm - 1;
+                                pc = pc + imm_B - 1;
                             }
                             break;
                     }
                     break;
-                case 0x37: // lui
 
-                    rd = (instr >> 7) & 0x01f;
-                    rs1 = (instr >> 15) & 0x01f;
-                    imm = (instr >> 20);
-                    reg[rd] = reg[rs1] + (imm << 20);
+                case 0x67: // Opcode 1100111 JALR - Jump and Link Register
+                    reg[rd] = pc;
+                    pc = reg[rs1] + imm;
                     break;
+
+                case 0x6f: // Opcode 1101111 JAL - Jump and Link
+                    reg[rd] = pc;
+                    pc = imm_J;
+                    break;
+
                 case 0x73: // ecall
                     if (reg[10] == 10){
                         System.out.println("DONE");
@@ -188,7 +248,7 @@ public class Simulator {
                     break;
             }
 
-            ++pc; // We count in 4 byte words
+
             if (pc >= progr.length) {
                 break;
             }
